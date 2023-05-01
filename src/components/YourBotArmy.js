661d8d5255ac37da924eee5f03c9480cc2654e23
Stephen <skaranja654@gmail.com>
2023-05-01 07:30:14 +0300

    adding selected bots

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,24 +1,21 @@
 import React from "react";
+import "./YourBotArmy.css";
 
 function YourBotArmy(props) {
   return (
-    <div
-      className="YourBotArmy"
-      style={{
-        border: "solid 1px grey",
-        height: "550px",
-        backgroundColor: "#b0e400",
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "center",
-        alignItems: "center",
-        gap: "10px",
-      }}
-    >
+    <div id="container">
       {props.bots.map((bot) => (
-        <div key={bot.id}>
+        <div
+          onClick={() => props.onRemoveBot(bot.id)}
+          id="YourBotArmy"
+          key={bot.id}
+        >
           <img src={bot.avatar_url} alt={bot.name} />
-          <p>{bot.name}</p>
+          <p id="yname">{bot.name}</p>
+          <p>{bot.bot_class}</p>
+          <p>damage:{bot.damage}</p>
+          <p>health:{bot.health}</p>
+          <p>armor:{bot.armor}</p>
         </div>
       ))}
     </div>
